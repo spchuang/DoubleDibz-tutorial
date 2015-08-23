84120a7b6628a716f1ae27a32eb247669c175d84
Samping Chuang <sampingchuang@gmail.com>
2015-08-22 17:49:38 -0700

    add totorial: set up flast app part 1

---
+++ setup_flask_app_part1/app/api/helloworld.py
@@ -0,0 +1,17 @@
+"""
+ Simple API endpoint for returning helloworld
+"""
+from flask import (Blueprint, render_template, current_app, request,
+                   flash, url_for, redirect, session, abort, jsonify, make_response)
+
+
+helloworld = Blueprint('auth', __name__, url_prefix='/api/helloworld')
+
+   
+@helloworld.route('/helloworld', methods=['GET'])
+def index():
+   
+   data = {
+      'message' : "helloworld"
+   }      
+   return make_response(jsonify(data))
---
+++ setup_flask_app_part1/app/common/constants.py
@@ -0,0 +1,4 @@
+import os
+
+# Instance folder path, make it independent.
+INSTANCE_FOLDER_PATH = os.path.join('/tmp', 'instance')
